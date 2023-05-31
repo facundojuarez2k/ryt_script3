7672a6e1f65b848c9f80c78638b8304d04b8e6be
Facundo Juarez <facundojuarezf@outlook.com>
2023-05-30 22:37:09 -0300

    Add tcp query method

--- dnstrace.py
+++ dnstrace.py
@@ -1,8 +1,9 @@
 import logging
+import random
 # Suprimir warnings en stdout
 logging.getLogger("scapy.runtime").setLevel(logging.ERROR)
 import scapy.all as scapy
-from scapy.all import IP, UDP, DNS, DNSQR
+from scapy.all import IP, UDP, DNS, TCP, DNSQR
 
 
 def main():
@@ -17,6 +18,29 @@ def main():
 
     print(answer[DNS].summary())
 
+def send_tcp_query():
+    ip_dst = "8.8.8.8"
+    port_dst = 53
+    
+    received_ack, received_seq = handshake(ip_dst, port_dst)
+
+    ip_packet = IP(dst=ip_dst)
+    tcp_segment = TCP(sport=port_dst, dport=port_dst, flags="PA", seq=received_ack, ack=received_seq + 1)
+    dns_request = DNS(rd=1, qd=DNSQR(qname = "example.com", qtype="A"))
+
+    answers, _ = scapy.sr(ip_packet / tcp_segment / dns_request, timeout=3, multi=1)
+
+    print(answers[DNS][0])
+
+def handshake(ip_address, port) -> tuple:
+    ip_packet = IP(dst=ip_address)
+    tcp_segment = TCP(sport=random.randint(49152, 65535), dport=port, flags="S", seq=random.randint(0, 2**32 - 1))
+    syn = ip_packet/tcp_segment
+    syn_ack = scapy.sr1(syn)
+    ack =   ip_packet/TCP(sport=syn_ack.dport, dport=port, flags="A", seq=syn_ack.ack, ack=syn_ack.seq + 1)
+    scapy.send(ack)
+    
+    return (syn_ack.ack, syn_ack.seq)
 
 if __name__ == '__main__':
     main()
