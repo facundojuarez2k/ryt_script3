7c2277fb162f0c2d4386148c359c5c98a50eb486
Facundo Juarez <facundojuarezf@outlook.com>
2023-06-17 17:15:27 -0300

    Agrega test solucionador local

--- dnstrace.py
+++ dnstrace.py
@@ -1,21 +1,24 @@
+from scapy.layers.dns import dnstypes, dnsclasses
+from scapy.all import IP, UDP, DNS, DNSQR
+import scapy.all as scapy
 import random
-import logging
 import argparse
 import re
 import sys
+import logging
 # Suprimir warnings en stdout
 logging.getLogger("scapy.runtime").setLevel(logging.ERROR)
-from scapy.all import IP, UDP, DNS, DNSQR
-import scapy.all as scapy
 # Importar diccionario de DNS record types y classes
-from scapy.layers.dns import dnstypes, dnsclasses
 
-DEFAULT_DNS = "8.8.8.8"
+FALLBACK_DNS = "8.8.8.8"
+
 
 def main():
-    
-    #resolver_address: str = get_main_nameserver() or DEFAULT_DNS
-    resolver_address: str = DEFAULT_DNS
+
+    resolver_address: str = get_main_nameserver()
+
+    if resolver_address is None or test_resolver == False:
+        resolver_address = FALLBACK_DNS
 
     try:
         args = parse_args()
@@ -24,8 +27,9 @@ def main():
     except ValueError as ex1:
         print(f'ERROR: {str(ex1)}', file=sys.stderr)
         sys.exit(1)
-    
+
     trace(fqdn, resolver_address)
+
 
 def trace(fqdn: str, dns_address: str) -> None:
     """Imprime la traza de la consulta iterativa para el fqdn indicado en el argumento
@@ -49,20 +53,20 @@ def trace(fqdn: str, dns_address: str) -> None:
     #######################################################################
     # Obtener registros NS de los servidores root usando el solucionador DNS inicial #
     #######################################################################
-    
+
     query = "."
     dns_answer = resolve_dns(query, dns_address, "NS", True)
 
     if dns_answer is None or dns_answer.ancount == 0:
         print(f'No NS records found for query "{query}"')
         return None
-    
+
     ns_records = parse_ns_records(dns_answer, dns_address)
 
     if len(ns_records) == 0:
         print(f'No NS records found for query "{query}"')
         return None
-    
+
     # Concatenar los resultados a la string de salida
     for k, v in ns_records.items():
         trace_string += f'{v["rrname"]}    {v["rclass"]}    {v["rtype"]}    {v["rdata"]}    {v["a_record"]}\n'
@@ -70,7 +74,7 @@ def trace(fqdn: str, dns_address: str) -> None:
     trace_string += divider(f'Response from {dns_address}')
 
     # Seleccionar un servidor TLD aleatoriamente
-    next_ns_fqdn: str    = random.choice(list(ns_records.keys()))
+    next_ns_fqdn: str = random.choice(list(ns_records.keys()))
     next_ns_address: str = ns_records[next_ns_fqdn]["a_record"]
 
     ###################################################################
@@ -91,22 +95,22 @@ def trace(fqdn: str, dns_address: str) -> None:
         ns_records = parse_ns_records(dns_answer, dns_address)
 
         if len(ns_records) == 0:
-            print(f'No NS records found for query "{qname}"')
             break
-        
+
         # Concatenar los resultados a la string de salida
         for k, v in ns_records.items():
             trace_string += f'{v["rrname"]}    {v["rclass"]}    {v["rtype"]}    {v["rdata"]}    {v["a_record"]}\n'
 
-        trace_string += divider(f'Response from {next_ns_address} ({next_ns_fqdn})')
+        trace_string += divider(
+            f'Response from {next_ns_address} ({next_ns_fqdn})')
 
-        next_ns_fqdn: str    = random.choice(list(ns_records.keys()))
+        next_ns_fqdn: str = random.choice(list(ns_records.keys()))
         next_ns_address: str = ns_records[next_ns_fqdn]["a_record"]
 
     # Obtener todos los RR para el FQDN a partir del nameserver autoritativo
     query = ".".join(split_fqdn)
     dns_answer = resolve_dns(query, next_ns_address, "A", False)
-    
+
     for x in range(dns_answer.ancount):
         record_type = dnstypes[dns_answer.an[x].type]
         record_class = dnsclasses[dns_answer.an[x].rclass]
@@ -114,18 +118,13 @@ def trace(fqdn: str, dns_address: str) -> None:
         record_data = dns_answer.an[x].rdata
         if type(record_data) == bytes:
             record_data = record_data.decode("utf-8")
-        
+
         trace_string += f'{query}     {record_class}      {record_type}      {record_data}\n'
 
-    trace_string += divider(f'Response from {next_ns_address} ({next_ns_fqdn})')
+    trace_string += divider(
+        f'Response from {next_ns_address} ({next_ns_fqdn})')
 
     print(trace_string)
-
-def divider(content: str) -> str:
-    string = "-------------------------------------------------------\n"
-    string += f'{content}\n'
-    string += "-------------------------------------------------------\n"
-    return string
 
 def resolve_dns(query: str, dns_address: str, record_type: str, recursive_lookup: bool = False, record_class: str = "IN") -> any:
     """Retorna la respuesta de Scapy para una consulta directa a un servidor DNS
@@ -140,21 +139,21 @@ def resolve_dns(query: str, dns_address: str, record_type: str, recursive_lookup
     Returns:
         any: Objeto de clase scapy.layers.dns.DNS con la respuesta obtenida
     """
-    
+
     ip_packet = IP(dst=dns_address)
     udp_segment = UDP(sport=scapy.RandShort(), dport=53)
     dns_query = DNS(
-        rd=recursive_lookup, 
+        rd=recursive_lookup,
         qd=DNSQR(qname=query, qtype=record_type, qclass=record_class),
     )
 
     answer = scapy.sr1(ip_packet/udp_segment/dns_query, verbose=0, timeout=5)
 
     if answer is None:
-        print("Request timeout")
         return None
-    
+
     return answer[DNS]
+
 
 def parse_ns_records(dns_answer: any, resolver_address: str) -> dict:
     """Retorna un diccionario con el fqdn del nameserver como clave y los campos del registro como valor.
@@ -175,7 +174,7 @@ def parse_ns_records(dns_answer: any, resolver_address: str) -> dict:
     query_name = dns_answer.qd[0].qname.decode("utf-8")
 
     for x in range(dns_answer.nscount):
-        
+
         record_name = dns_answer.ns[x].rrname.decode("utf-8")
 
         if record_name != query_name:
@@ -192,7 +191,7 @@ def parse_ns_records(dns_answer: any, resolver_address: str) -> dict:
             "rdata": record_data,
             "a_record": None
         }
-    
+
     for x in range(dns_answer.ancount):
         record_name = dns_answer.an[x].rrname.decode("utf-8")
         record_type = dnstypes[dns_answer.an[x].type]
@@ -223,6 +222,7 @@ def parse_ns_records(dns_answer: any, resolver_address: str) -> dict:
 
     return output
 
+
 def parse_args() -> object:
     """Captura y retorna los argumentos del programa
 
@@ -238,7 +238,6 @@ def parse_args() -> object:
 
     return parser.parse_args()
 
-# Helpers #
 
 def validate_args(args: object) -> None:
     """Valida los argumentos del programa
@@ -251,7 +250,8 @@ def validate_args(args: object) -> None:
     """
     if is_valid_fqdn_syntax(args.fqdn) is False:
         raise ValueError(f'Value {args.fqdn} is not a valid FQDN string.')
-    
+
+
 def is_valid_fqdn_syntax(value: str) -> bool:
     """Retorna True si value representa un FQDN
 
@@ -261,9 +261,11 @@ def is_valid_fqdn_syntax(value: str) -> bool:
     Returns:
         bool: True si value es un FQDN, False caso contrario.
     """
-    valid_string = re.compile(r"(?=^.{4,253}\.?$)(^((?!-)[a-zA-Z0-9-]{1,63}(?<!-)\.)+[a-zA-Z]{2,63}\.?$)")
+    valid_string = re.compile(
+        r"(?=^.{4,253}\.?$)(^((?!-)[a-zA-Z0-9-]{1,63}(?<!-)\.)+[a-zA-Z]{2,63}\.?$)")
     match = re.fullmatch(valid_string, value)
     return match is not None
+
 
 def test_fqdn_tld(fqdn: str, resolver_address: str):
     """Verifica que el TLD del FQDN sea válido
@@ -277,9 +279,10 @@ def test_fqdn_tld(fqdn: str, resolver_address: str):
     """
     tld = fqdn.split(".")[-1] + "."
     dns_answer = resolve_dns(tld, resolver_address, "NS", True)
-    
+
     if dns_answer.ancount == 0:
         raise ValueError("FQDN no válido")
+
 
 def get_main_nameserver() -> any:
     """Retorna el primer nameserver del archivo /etc/resolv.conf
@@ -291,16 +294,36 @@ def get_main_nameserver() -> any:
         with open("/etc/resolv.conf", encoding="utf-8") as config_file:
             for line in config_file.readlines():
                 line = line.lstrip()
-                
+
                 if line.startswith("#"):
                     continue
-        
+
                 if "nameserver" in line:
                     ns_list.append(line.split()[1])   # Extraer la dirección IP
-        
+
         return ns_list[0] if len(ns_list) > 0 else None
     except Exception:
         return None
+
+
+def test_resolver(resolver_address: str) -> bool:
+    """Verifica que el cliente DNS del argumento funcione correctamente.
+
+    Args:
+        resolver_address (str): Dirección IPv4 del solucionador
+
+    Returns:
+        bool: True si el solucionador DNS obtiene una respuesta para una consulta de prueba. Falso caso contrario.
+    """
+    answer = resolve_dns("google.com", resolver_address, "A", True)
+    return answer is not None
+
+
+def divider(content: str) -> str:
+    string = "-------------------------------------------------------\n"
+    string += f'{content}\n'
+    string += "-------------------------------------------------------\n"
+    return string
 
 if __name__ == "__main__":
     main()
